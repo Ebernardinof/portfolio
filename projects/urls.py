3c57edb3a204a40788d99873d54f07850a10383f
Ebernardinof <elisabetebernardino@gmail.com>
2020-03-01 06:38:03 +0000

    Protfolio app

---
+++ projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+
+# app_name = 'projects'
+urlpatterns = [
+    path('', views.project_index, name='project_index'),
+    path('<int:pk>/', views.project_detail, name='project_detail'),
+
+]
